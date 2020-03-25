02847815dc47e63fd23c0004b49bcf238ea71be4
Giovanni De Luca <ginholucacorrea@icloud.com>
2020-03-25 19:13:33 -0300

    backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier'],
+
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    semi: 'off',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+}
